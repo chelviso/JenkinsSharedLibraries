d73427455259e07478db4fb61006dbc37790aed8
chelviso <111324611+chelviso@users.noreply.github.com>
2022-10-02 12:13:26 -0500

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -14,6 +14,18 @@ def call(String stageName){
      }
   else if ("${stageName}" == "Upload Into Tomcat")
      {
-       deploy adapters: [tomcat9(credentialsId: 'tomcatcred', path: '', url: 'http://18.207.212.227:8080/')], contextPath: null, war: 'target/*war'
+       deploy adapters: [tomcat9(credentialsId: 'tomcatcred', path: '', url: 'http://54.197.97.134:8080/')], contextPath: null, war: 'target/*war'
      }
+   else if ("${stageName}" == "Waiting Approval")
+     {
+      timeout(time:5, unit:'DAYS') {
+    input message: 'Application ready for deployment, Please review and approve'
+
+}
+     }
+  else if ("${stageName}" == "Deploy To Production")
+     {
+       deploy adapters: [tomcat9(credentialsId: 'tomcat2cred', path: '', url: 'http://44.211.199.201:8080/')], contextPath: null, war: 'target/*war'
+     }
+  
 }
